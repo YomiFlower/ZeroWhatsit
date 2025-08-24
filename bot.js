cc153aee6d7f5da3b422645c43d47293d9871968
YomiFlower <samkenyon272@gmail.com>
2025-08-24 15:43:34 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -24,3 +24,5 @@ app.listen(3000, () => console.log("API running on port 3000"));
 // login
 client.login(process.env.DISCORD_TOKEN);
 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`API running on port ${PORT}`));
